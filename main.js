53def63c7ef0311ae0bdae50da4f1c3501d67b79
Junwhy420 <junwai95@outlook.com>
2021-10-24 13:00:34 +0800

    Updates

---
+++ main.js
@@ -0,0 +1,106 @@
+import './style.css'
+
+import * as THREE from 'three';
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+
+
+});
+
+renderer.setPixelRatio( window.devicePixelRatio);
+renderer.setSize( window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100 ) 
+const material = new THREE.MeshStandardMaterial ( {color: 0xFF6347, });
+const torus = new THREE.Mesh ( geometry, material );
+
+scene.add(torus)
+
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set(20,20,20)
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+const gridHelper = new THREE.GridHelper(200, 50);
+const controls = new OrbitControls(camera, renderer.domElement);
+
+
+scene.add(pointLight, ambientLight, gridHelper)
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial( {color: 0xffffff})
+  const star = new THREE.Mesh( geometry, material );
+
+  const [x, y, z]= Array (3).fill().map(() => THREE.MathUtils.randFloatSpread( 100 ) );
+
+  star.position.set(x, y, z);
+  scene.add(star)
+}
+
+
+Array (200).fill().forEach(addStar)
+
+const loader = new THREE.TextureLoader();
+scene.background = loader.load( 'https://threejs.org/examples/textures/terrain/grasslight-big.jpg' );
+
+const kanyeTexture = new THREE.TextureLoader().load('kanye-west-2-copy.jpg');
+const kanye = new THREE.Mesh(
+  new THREE.BoxGeometry(3,3,3),
+  new THREE.MeshBasicMaterial({map: kanyeTexture })
+  
+);
+
+    
+    scene.add(kanye);
+
+    // MOON
+
+    const moonTexture = new THREE.TextureLoader().load('moon.jpg');
+    const normalTexture = new THREE.TextureLoader().load('normal.jpg');
+
+    const moon = new THREE.Mesh(
+      new THREE.SphereGeometry(3, 32, 32),
+      new THREE.MeshStandardMaterial( { 
+        map: moonTexture,
+        normalMap: normalTexture
+      })
+    );
+
+    scene.add(moon);
+
+
+
+
+
+
+function animate(){
+  requestAnimationFrame ( animate );
+
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+
+  controls.update();
+
+
+
+
+
+
+  renderer.render( scene, camera );
+
+}
+ animate()
+
